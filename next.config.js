2d08aab86195603133f0423bd182d442d81fa9e9
nikowidjaja-code <npwidjaja@gmail.com>
2025-05-18 03:46:50 +0700

    revert: prev working version

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-      experimental: {
-            serverActions: true,
-      },
-      // Ensure API routes are properly handled in production
-      output: 'standalone',
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
