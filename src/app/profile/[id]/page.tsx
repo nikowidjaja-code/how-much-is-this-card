5b6db15f92eece1cfb025971e4c447278f3d36e9
nikowidjaja-code <npwidjaja@gmail.com>
2025-06-04 15:06:55 +0700

    feat: add maxLength to input user name

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -242,6 +242,7 @@ export default function ProfilePage({ params }: { params: { id: string } }) {
                             onChange={(e) => setEditedName(e.target.value)}
                             className="px-3 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                             placeholder="Enter your name"
+                            maxLength={24}
                           />
                           <button
                             onClick={handleUpdateProfile}
